b80259672675dae1fc2bf8b20f1693bd0d08d5c9
Iegor Maiorov <maiorov.prog@gmail.com>
2018-06-28 20:23:02 +0300

    initial commit

---
+++ blog-app/src/index.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends Component {
+    render() {
+        return (<div>Hello</div>)
+    }
+}
+
+class GoodBye extends Component {
+    render() {
+        return (<div>Goodbye!</div>)
+    }
+}
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+        <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/bye" component={GoodBye}/>
+        </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
